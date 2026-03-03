9173fd4e74c50fb968e456fac6e632f538de7385
redpangilinan <82772769+redpangilinan@users.noreply.github.com>
2026-03-03 08:17:42 +0800

    chore(migrations): nuke migrations before release

--- packages/db/src/migrations/0013_stale_jasper_sitwell.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM "bug_report" WHERE "submission_status" = 'pending_upload';--> statement-breakpoint
-DROP TABLE "bug_report_storage_cleanup" CASCADE;--> statement-breakpoint
-ALTER TABLE "bug_report" DROP COLUMN "attachment_url";--> statement-breakpoint
-ALTER TABLE "bug_report" DROP COLUMN "attachment_key";
